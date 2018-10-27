d9b49d34e0e94ea4aef16facbb8067000be7911c
Sanghun Park <psanghun@gmail.com>
2018-10-28 01:30:47 +0900

    chapter10 complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,83 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends Component {
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: '시은이 만세~!!!', done: true },
+      { id: 1, text: '초은아 뭐하니?', done: false }
+    ]
+  };
+
+  handleChange = e => {
+    const { value } = e.target;
+    this.setState({
+      input: value
+    });
+  };
+
+  id = 1;
+  getId = () => {
+    return ++this.id;
+  };
+
+  handleInsert = e => {
+    const { todos, input } = this.state;
+
+    const newTodo = {
+      text: input,
+      done: false,
+      id: this.getId()
+    };
+
+    this.setState({
+      todos: [...todos, newTodo],
+      input: ''
+    });
+  };
+
+  handleToggle = id => {
+    const { todos } = this.state;
+    const index = todos.findIndex(todo => todo.id === id);
+    const toggled = {
+      ...todos[index],
+      done: !todos[index].done
+    };
+
+    this.setState({
+      todos: [
+        ...todos.slice(0, index),
+        toggled,
+        ...todos.slice(index + 1, todos.length)
+      ]
+    });
+  };
+
+  handleRemove = id => {
+    const { todos } = this.state;
+    const index = todos.findIndex(todo => todo.id === id);
+
+    this.setState({
+      todos: [...todos.slice(0, index), ...todos.slice(index + 1, todos.length)]
+    });
+  };
+
   render() {
+    const { input, todos } = this.state;
+    const { handleChange, handleInsert, handleToggle, handleRemove } = this;
+
     return (
       <PageTemplate>
-        <TodoInput />
-        <TodoList />
+        <TodoInput
+          onChange={handleChange}
+          onInsert={handleInsert}
+          value={input}
+        />
+        <TodoList
+          todos={todos}
+          onToggle={handleToggle}
+          onRemove={handleRemove}
+        />
       </PageTemplate>
     );
   }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,12 +3,18 @@ import TodoItem from '../TodoItem';
 
 class TodoList extends Component {
   render() {
-    return (
-      <div>
-        <TodoItem done>시은이 만세!</TodoItem>
-        <TodoItem>초은아 뭐하니?</TodoItem>
-      </div>
-    );
+    const { todos, onToggle, onRemove } = this.props;
+    const todolist = todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        onToggle={() => onToggle(todo.id)}
+        done={todo.done}
+        onRemove={() => onRemove(todo.id)}
+      >
+        {todo.text}
+      </TodoItem>
+    ));
+    return <div>{todolist}</div>;
   }
 }
 
